bc714b066e63c9c187fc788573b4dbfac1289c73
Dixneuf Baptiste <baptiste.dixneuf@gmail.com>
2017-03-17 09:55:21 +0100

    Create and Edit the entity TypePoint

--- WUI/Controllers/TypePointController.cs
+++ WUI/Controllers/TypePointController.cs
@@ -19,6 +19,7 @@ namespace WUI.Controllers
         {
         }
 
+
         // GET: /TypePoint/
         [Authorize(Roles = "Administrateur")]
         public ActionResult Index()
@@ -27,6 +28,77 @@ namespace WUI.Controllers
             return View(result);
         }
 
+        //
+        // GET: /TypePoint/Create
+        [Authorize(Roles = "Administrateur")]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /TypePoint/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(TypePointModel typePoint)
+        {
+            try
+            {
+                if (ModelState.IsValid && MgtTypePoint.GetInstance().AddTypePoint(typePoint.ToBo()))
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    return View();
+                }
+
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+
+        //
+        // GET: /TypePoint/Edit/{id}
+        [Authorize(Roles = "Administrateur")]
+        public ActionResult Edit(int id = 0)
+        {
+            var result = MgtTypePoint.GetInstance().GetTypePoint(id).ToModel();
+            if (result == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(result);
+        }
+
+
+        //
+        // POST: /TypePoint/Edit/{id}
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(TypePointModel typePoint)
+        {
+            try
+            {
+                var result = MgtTypePoint.GetInstance().UpdateTypePoint(typePoint.ToBo());
+                if (result)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    return View();
+                }
+            }
+            catch
+            {
+                return View();
+            }
+        }
 
     }
 }
